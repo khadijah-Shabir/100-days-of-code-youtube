f8dd508a1af4d8321e6b00e26b8fe552cb9b91ac
Khadijah Shabir <133489629+khadijah-Shabir@users.noreply.github.com>
2024-06-29 11:40:45 +0500

    Update main.py

--- 28-Day28-f-strings/main.py
+++ 28-Day28-f-strings/main.py
@@ -2,6 +2,7 @@ letter = "Hey my name is {1} and I am from {0}"
 country = "India"
 name = "Harry"
 
+
 print(letter.format(country, name))
 print(f"Hey my name is {name} and I am from {country}")
 print(f"We use f-strings like this: Hey my name is {{name}} and I am from {{country}}")
